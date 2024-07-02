263e943943f6c52bb5ad38487c7817d11b319153
Peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2024-07-02 02:21:34 -0300

    :zap: [50% completed.]

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {ChakraBaseProvider} from "@chakra-ui/react";
+import {ChakraBaseProvider, extendTheme} from "@chakra-ui/react";
 import MyRoutes from './routes';
+
+const theme = extendTheme({
+    colors: {
+        doc: {
+            'primary-color': "#0E0D33",
+            "gradient-one:": "#812DE2",
+            "gradient-two": "#423EE0"
+        },
+    },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraBaseProvider>
+    <ChakraBaseProvider theme={theme}>
         <MyRoutes/>
     </ChakraBaseProvider>
   </React.StrictMode>,
--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,10 +1,33 @@
-import {Flex} from "@chakra-ui/react";
+import {Box, Button, Circle, Flex, Heading, HStack, Text, VStack} from "@chakra-ui/react";
 
 const LandingPage = () => {
     return(
-        <Flex w={'full'} h={"100vh"} bg={"red"}>
+        <VStack w={'full'} h={"100vh"} bg={"doc.primary-color"}>
+            <HStack w={"full"} align={"end"} justifyContent={"end"}>
+                <Circle size={"256px"}
+                        bgGradient={"linear(to-r,#812DE2, #423EE0)"}
+                        marginY={"-128px"}
+                        marginX={"-32px"}
+                        filter={"auto"}
+                        blur={"120px"}
+                />
+            </HStack>
 
-        </Flex>
+            <HStack justifyContent={"center"} align={"center"} h={"full"}>
+                <VStack as={"section"}>
+                    <Heading fontSize={"64px"} bgGradient={"linear(to-r,#9130F4,#4646F9)"} bgClip={"text"}>Document Hunt</Heading>
+                    <Heading fontSize={"64px"} color={"white"}>Seguro e rapido</Heading>
+                    <Box width={"450px"} h={"128px"}>
+                        <Text color={"white"} textAlign={"justify"}>
+                            Cansado de ficar procurando documentos e/ou links de pesquisa a mão ? Nós temos a solução para você. Nossa plataforma oferece uma ferramenta de procura nunca antes vista. Clique no botão abaixo e faça sua primeira procura.
+                        </Text>
+                    </Box>
+                    <Button alignContent={"start"} color={"#fff"} variant={"outline"}>Consultar</Button>
+
+                </VStack>
+            </HStack>
+
+        </VStack>
     );
 }
 
