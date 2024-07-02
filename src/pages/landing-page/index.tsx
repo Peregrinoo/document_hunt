974cafc77f897f73b99c492696828e90954429ce
Peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2024-07-02 01:47:20 -0300

    :sparkles: [Creating the landing-page file.]

---
+++ src/pages/landing-page/index.tsx
@@ -0,0 +1,11 @@
+import {Flex} from "@chakra-ui/react";
+
+const LandingPage = () => {
+    return(
+        <Flex w={'full'} h={"100vh"} bg={"red"}>
+
+        </Flex>
+    );
+}
+
+export default LandingPage;
