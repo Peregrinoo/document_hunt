8987d1ab4d1a66071370e2d76cc4cf8dfaa278a7
Peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2024-07-02 01:48:18 -0300

    :zap: [Added landinpage component to routes files.]

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import LandingPage from "./pages/landing-page";
 
 const myRoutes = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route/>
+                <Route element={<LandingPage/>} path={"/"}/>
             </Routes>
         </BrowserRouter>
     )
